a747c5e9dbb89c17e545621c5e4c0f62059fca8b
Bodron Cosmin <bodroncosmin98@gmail.com>
2023-08-16 20:19:25 +0300

    deleye

--- CinemaSky/js/faqs.js
+++
@@ -1,7 +0,0 @@
-const toggles1 = document.querySelectorAll('.faq-toggle')
-
-toggles1.forEach((toggle1) => {
-  toggle1.addEventListener('click', () => {
-    toggle1.parentNode.classList.toggle('active')
-  })
-})
